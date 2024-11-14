af4ae3d094e0541d7b1d55c447529d4f596ba42a
mifamira <150776819+mifamira@users.noreply.github.com>
2024-11-14 09:07:54 +0700

    Add files via upload

---
+++ forms/newsletter.php
@@ -0,0 +1,27 @@
+<?php
+  $receiving_email_address = '22082010147@student.upnjatim.ac.id';
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject ="New Subscription: " . $_POST['email'];
+
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+
+  $contact->add_message( $_POST['email'], 'Email');
+
+  echo $contact->send();
+?>
